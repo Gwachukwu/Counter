772b4b05d776f4df53a7c7bc8bef46a86b48de56
gwachukwu <gwachukwuelebeke@gmail.com>
2024-06-15 21:35:07 -0400

    Beautify a bit with space and padding

--- composeApp/src/androidMain/kotlin/com/jetbrains/spacetutorial/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/jetbrains/spacetutorial/MainActivity.kt
@@ -1,34 +1,40 @@
 package com.jetbrains.spacetutorial
 
-import App
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            Counter()
         }
     }
 }
 
-@Preview
-@Composable
-fun AppAndroidPreview() {
-    App()
-}
+//@Preview
+//@Composable
+//fun AppAndroidPreview() {
+//    App()
+//}
 
 @Preview
 @Composable
@@ -37,11 +43,17 @@ fun Counter(){
         mutableIntStateOf(0)
     }
     Column {
-       Row {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
            Text(count.intValue.toString())
        }
         Row {
             Increment(count)
+            Spacer(modifier = Modifier.width(16.dp))
             Decrement(count)
         }
     }
